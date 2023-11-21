d5b056de95e4edb31a25ba7ebbdb15ef4dd5b462
Yarik <cahloyaroslav@gmail.com>
2023-11-21 16:25:09 +0200

    Add simple AI

--- Source/JustShoot/JustShootProjectile.cpp
+++ Source/JustShoot/JustShootProjectile.cpp
@@ -34,9 +34,12 @@ AJustShootProjectile::AJustShootProjectile()
 void AJustShootProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
 	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+		if(OtherComp->IsSimulatingPhysics())
+		{
+			OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
+		}
 
 		Destroy();
 	}
